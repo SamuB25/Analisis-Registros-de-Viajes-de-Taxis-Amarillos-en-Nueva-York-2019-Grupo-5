cf4d44c88ffaa630829f4688f6dbb6247fd89d63
SamuB25 <sbarreto0502@gmail.com>
2026-04-11 17:46:14 -0400

    Creacion de grafico en la app
    se creo un grafico de prueba en el streamlit para probar el correcto funcionamiento del src y ver la hora pico de los viajes en taxi

--- App_taxis_ny/src/query_manager.py
+++ App_taxis_ny/src/query_manager.py
@@ -5,7 +5,7 @@ import os
 class query_manager:
     def __init__(self,db_path="taxis_ny.db"):
         # Creamos la conexión que se va a utilizar con los archivos.A parte de que se creara el archivo de "taxis.ny.db"
-        self.con = duckdb.connect(db.path)
+        self.con = duckdb.connect(db_path)
         self._register_tables()
 
     def _register_tables(self):
@@ -13,14 +13,14 @@ class query_manager:
         # Comenzando con la ruta de los archivos.
         dir_data = os.path.dirname(os.path.abspath(__file__)) # Mediante la ubicación de este archivo coordinamos la ruta
         # de forma en que dir_data sepa que debe trabajar en la carpeta "src", con el join le indicamos que avance en esa carpeta y entre a "data".
-        ruta_data = os.path.joi(base_dir, "..", "data")
+        ruta_data = os.path.join(dir_data, "..", "..", "data")
 
         # Creamos un diccionario que identifique el nombre de la tabla y su archivo correspondiente.
 
         tables = {
             "finanzas":"finanzas_viaje.parquet",
             "locaización":"localizacion_viaje.parquet",
-            "pagos","metodo_pago.parquet",
+            "pagos":"metodo_pago.parquet",
             "plataformas":"plataformas_tpep.parquet",
             "viaje":"registro_viaje.parquet",
             "tarifas":"tarifas.parquet"
@@ -33,11 +33,11 @@ class query_manager:
         # Creamos un leve filtro que verifique la existencia del archivo en el dispositivo.
 
             if os.path.exists(ruta_archive):
-                self.con.execute(f"CREATE OR REPLACE VIEW {name_table}. AS SELECT * FROM read.parquet("{ruta_archive}") ")
-                print(f"Tabla {nombre_tabla} esta lista")
+                self.con.execute(f"CREATE OR REPLACE VIEW {name_table} AS SELECT * FROM read_parquet('{ruta_archive}')")
+                print(f"Tabla {name_table} esta lista")
             else:
                 print(f" ADVERTENCIA: No se encontró ´{archive}´ en la carpeta data.")
-        print("Configuraciones de registro finalizadas.").
+        print("Configuraciones de registro finalizadas.")
     
     def execute_query(self, sql):
         # Para que los querys se ejecuten desde sql y devuelva un dataframe
