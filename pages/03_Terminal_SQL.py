4b1324340eb7b4a197f73df8150c6e7461f29c5f
SamuB25 <sbarreto0502@gmail.com>
2026-04-12 14:35:37 -0400

    agrege las preguntas del custionario

---
+++ pages/03_Terminal_SQL.py
@@ -0,0 +1,47 @@
+# Cuestionario SQL 
+
+# 1. Cuartiles de Distancia y Comportamiento de Propinas
+# Enunciado: Divide todos los viajes pagados con tarjeta de crédito en 4 cuartiles basados 
+# en la distancia del viaje. Para cada cuartil, calcula la distancia promedio, el monto de tarifa
+#  promedio y el porcentaje promedio que representa la propina respecto al monto total.
+# Muestra el número de cuartil y las métricas calculadas.
+
+
+
+
+
+# 2. Análisis de Embotellamientos Críticos por Hora
+# Enunciado: Identifica a qué hora del día la ciudad sufre los peores embotellamientos. Define
+# un embotellamiento como un viaje de más de 1 milla de distancia donde la velocidad promedio
+# fue inferior a 5 millas por hora. Agrupa los datos por la hora del día (0 a 23) de la recogida.
+# Muestra la hora, el número total de viajes realizados a esa hora, la cantidad de viajes que
+# cayeron en embotellamiento y el porcentaje que estos representan. Ordena de peor a mejor porcentaje.
+
+
+
+
+# 3. Crecimiento Intermensual (MoM) de la Demanda
+# Enunciado: Evalúa la estacionalidad del servicio calculando la tasa de crecimiento mensual
+# (Month-over-Month) en la cantidad total de viajes. Para cada mes del ultimo trimestre
+# del año 2019, muestra el mes, el total de viajes de ese mes, el total de viajes del mes
+# inmediatamente anterior y el porcentaje de variación.
+
+
+
+# 4. Comportamiento de Propinas: Fines de Semana vs. Días Laborables
+# Enunciado: ¿La gente da mejores propinas los fines de semana? Clasifica los viajes pagados
+# con tarjeta de crédito en ”Fin de Semana”(Sábado y Domingo) y ”Día Laborable”(Lunes a Viernes)
+# basándote en la fecha de recogida. Calcula para ambos grupos el número de viajes,
+# el promedio de la tarifa base y el porcentaje que representa la propina sobre el monto total.
+
+
+
+
+# 5. Impacto del Recargo por Congestión según la Longitud del Viaje
+# Enunciado: Analiza cómo afecta el recargo por congestión a la estructura de costos del
+# pasajero. Clasifica los viajes en Cortos”(menos de 2 millas), ”Medios”(entre 2 y 8 millas) y
+# ”Largos”(más de 8 millas). Para cada categoría, calcula el porcentaje promedio del monto
+# total que es consumido exclusivamente por el recargo de congestión. Excluye viajes donde el
+# recargo sea cero o nulo.
+
+
