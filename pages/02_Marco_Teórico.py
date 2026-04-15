0539de71101de78c4efdd941b8ffb35abe050ff4
SamuB25 <sbarreto0502@gmail.com>
2026-04-15 19:17:50 -0400

    agrege las paginas de la app a la carpeta pages

---
+++ pages/02_Marco_Teórico.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+
+
+# Titulo principal
+
+st.title("Análisis del comportamiento de los pasajeros en horas pico en el sistema de transporte Yellow Cars de Nueva York (Octubre-Diciembre 2019)")
+
+st.header("Marco teórico")
+
+st.subheader("Antecedentes")
+
