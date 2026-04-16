fd48f7a8b947699343a3f391776b89c026a6046e
SamuB25 <sbarreto0502@gmail.com>
2026-04-15 23:39:34 -0400

    Finalizacion del marco teorico y bibliografias
    Se termino de desarrollar lo faltante del marco teorico y todas las bibliografias usadas en el proyecto

--- pages/06_Bibliografías.py
+++ pages/06_Bibliografías.py
@@ -7,11 +7,13 @@ st.header("Bibliografías")
 st.markdown("""
 - New York City taxi trips. (2020). Kaggle. https://www.kaggle.com/datasets/dhruvildave/new-york-city-taxi-trips-2019
 - Major, Elizabeth. (2019). TLC Trip Records User Guide. https://www.nyc.gov/assets/tlc/downloads/pdf/trip_record_user_guide.pdf 
-- Gaurav Singh (2025). Pronóstico de la disminución del número de pasajeros de los taxis amarillos de Nueva York: un análisis de series temporales del número diario de pasajeros (2017-2019). https://arxiv.org/html/2507.10588
+- Gaurav Singh. (2025). Pronóstico de la disminución del número de pasajeros de los taxis amarillos de Nueva York: un análisis de series temporales del número diario de pasajeros (2017-2019). https://arxiv.org/html/2507.10588
 - Daniel Parra. (2019). Información importante: Licencias de taxi en Nueva York. https://citylimits.org/need-to-know-taxi-medallions-in-new-york/
 - INSHUR. (2025). Breve historia de los taxis en la ciudad de Nueva York. https://inshur.com/en-us/blog/a-brief-history-of-taxis-in-new-york-city
-- 
-- 
-- 
-- 
+-  Lisa Brown. (2015). Los taxis amarillos ahora tienen su propia aplicación para solicitar viajes. https://observer.com/2015/09/yellow-cabs-now-have-their-own-ride-hailing-app/
+- curb. (2026). Sobre nosotros. https://www.gocurb.com/about
+- Ayuntamiento de la Ciudad de Nueva York. (2020). Grupo de trabajo sobre licencias de taxi. https://council.nyc.gov/data/taxis/ 
+- Alanna Schubach. (2017). Arro y Curb: ¿Cómo se comparan las aplicaciones de taxi de Nueva York con Uber, Lyft y la forma tradicional de parar un taxi?. https://www.brickunderground.com/live/nyc-taxi-apps-arro-curb-uber
+- Hamid Moradi. (2021). Limpieza de datos del registro de viajes de un taxi de Nueva York - Python. https://cyclingcity.ir/data-cleaning-of-trip-record-of-new-york-taxi/
+- AWS.Amazon (2026). ¿Qué es el procesamiento analítico en línea (OLAP)?. https://aws.amazon.com/es/what-is/olap/
 """)
